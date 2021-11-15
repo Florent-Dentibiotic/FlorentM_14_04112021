ceb28254444783e455431c1a76c9780d8888c084
MacbookAir <fmdentiste@pm.me>
2021-11-15 12:41:36 +0100

    redux toolkit

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,8 @@
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import employeeReducer from './reducers/employeesReducer';
 
-const reduxDevtools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const reducer = combineReducers({
-    employee: employeeReducer,
+export const store = configureStore({
+    reducer: {
+        employee: employeeReducer,
+    },
 });
-
-export const store = createStore(reducer, reduxDevtools);
