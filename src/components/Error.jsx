df640a92bc33129bdd71e50d270caf5858d75f40
MacbookAir <fmdentiste@pm.me>
2021-12-01 11:10:56 +0100

    error page design

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 export default function Error() {
     return (
         <>
-            <div className="font-sans relative opacity-60 bg-gradient-to-br from-green-400 via-blue-800 to-green-900 bg-cover bg-center w-screen h-screen flex justify-center items-center "></div>
-            <div className="absolute top-4 left-1/2 -ml-48 w-96 h-5/6 bg-white rounded overflow-hidden bg-scroll flex flex-col justify-center items-center">
+            <main className="font-sans relative opacity-10 bg-main-pattern bg-repeat-space bg-center w-screen h-screen flex justify-center items-center "></main>
+            <div className="absolute text-center border-4 border-green-900 border-opacity-90 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 bg-white rounded overflow-hidden bg-scroll p-8">
                 <div className="py-5">
                     <h1 className="font-sans text-3xl text-center">
                         Page non trouvée
                     </h1>
                 </div>
                 <Link
-                    className="font-sans text-xl text-center underline"
+                    className="font-sans text-xl text-center underline m-5"
                     to="/"
                 >
                     Accueil
