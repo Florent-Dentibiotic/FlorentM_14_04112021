428a005f934f8f2a4d4090cbd70d9d150d3ef3e2
MacbookAir <fmdentiste@pm.me>
2021-12-27 11:37:57 +0100

    main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Form from '../components/Form';
 import { useState } from 'react';
 //import Modal from '../components/Modal';
 import { Modal } from 'dentibiotics_modal';
+import guides from 'guides_js_by_raficraft';
 
 export default function Home() {
     const [modal, setModal] = useState(false);
@@ -14,14 +15,14 @@ export default function Home() {
 
     return (
         <>
-            <main className="font-sans relative opacity-10 bg-main-pattern bg-repeat-space bg-center w-screen h-screen flex justify-center items-center "></main>
-            <div className="absolute border-4 border-green-900 border-opacity-90 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 bg-white rounded overflow-hidden bg-scroll">
+            <div className="font-sans relative opacity-10 bg-main-pattern bg-repeat-space bg-center w-screen h-screen flex justify-center items-center "></div>
+            <main className="absolute border-4 border-green-900 border-opacity-90 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 bg-white rounded overflow-hidden bg-scroll">
                 <Header
                     link={'/employee-list'}
                     label={'View Current Employees'}
                 />
                 <Form setModal={setModal} setModalContent={setModalContent} />
-            </div>
+            </main>
             {modal && (
                 <Modal
                     backgroundAction={handleClick}
